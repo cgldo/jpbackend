ecc5f9c86657d1aaca9cf3eacdade0f186a0969d
unknown <3CuongXD@gmail.com>
2022-08-15 11:28:17 -0400

    final login backend

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,25 +3,27 @@ const User = require('../models/user.model')
 const router = require('express').Router()
 
 
-router.route('/new').post((req, res) => {
+router.route('/post').post((req, res) => {
     const newUser = new User(req.body)
     newUser.save()
         .then(user => res.json(user))
         .catch(err => res.status(400).json("Error! " + err))
 })
 
-router.route('/find/:username/password/:password').get((req, res) => {
+router.route('/get').get((req, res) => {
     // using .find() without a paramter will match on all user instances
     User.findOne({username:req.params.username, password:req.params.password})
         .then(result => res.json(result))
         .catch(err => res.status(400).json('Error! ' + err))
 })
 
-router.route('/delete/:id').delete((req, res) => {
-    User.deleteOne({ _id: req.params.id })
+router.route('/delete').delete((req, res) => {
+    User.deleteOne({ username: req.params.username })
         .then(success => res.json('Success! User deleted.'))
         .catch(err => res.status(400).json('Error! ' + err))
 })
 
 
 module.exports = router
+ 
+//test route http://localhost:5000/users
