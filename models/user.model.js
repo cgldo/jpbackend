94d0efc54d20040fdfeb732a4032f1d0c50b2334
unknown <3CuongXD@gmail.com>
2022-08-17 11:23:43 -0400

    first version of user score

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    score:  [{}] },
+    score:{} },
     {collection : 'user'})
 
 const User = mongoose.model('User', userSchema)
