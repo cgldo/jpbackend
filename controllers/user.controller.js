8c3398de0436b79d996c12c7908ea8825309416b
unknown <3CuongXD@gmail.com>
2022-08-17 11:15:32 -0400

    first version of user score

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,14 +12,21 @@ router.route('/post').post((req, res) => {
 
 router.route('/get').get((req, res) => {
     // using .find() without a paramter will match on all user instances
-    User.findOne({username:req.params.username, password:req.params.password})
+    User.findOne({username:req.body.username, password:req.body.password})
         .then(result => res.json(result))
         .catch(err => res.status(400).json('Error! ' + err))
 })
 
 router.route('/delete').delete((req, res) => {
-    User.deleteOne({ username: req.params.username })
+    User.deleteOne({ username: req.body.username, password:req.body.password})
         .then(success => res.json('Success! User deleted.'))
+        .catch(err => res.status(400).json('Error! ' + err))
+})
+
+router.route('/postscore').post((req, res) => {
+    User.updateOne({username:req.body.username, password:req.body.password}, { $set: {
+        score:req.body.score}})
+        .then(result => res.json(result))
         .catch(err => res.status(400).json('Error! ' + err))
 })
 
